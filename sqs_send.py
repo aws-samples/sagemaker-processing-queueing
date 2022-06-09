7e648702669f3c68181b29521e6edb7eee226e5d
bruklein@amazon.com <bruklein@amazon.com>
2022-06-09 10:41:47 -0400

    adds account_id param to sqs send

--- sqs_send.py
+++ sqs_send.py
@@ -4,12 +4,13 @@ import uuid
 
 if __name__ == '__main__':
     client = boto3.client('sqs')
+    account_id = boto3.client('sts').get_caller_identity()['Account']
     for i in range(10):
         send_message_resp = client.send_message(
-            QueueUrl='https://sqs.us-east-1.amazonaws.com/886385276223/SageMakerJobs.fifo',
+            QueueUrl=f'https://sqs.us-east-1.amazonaws.com/{account_id}/SageMakerJobs.fifo',
             MessageBody=json.dumps({
                 "ProcessingJobName": f"{uuid.uuid4()}",
-                "RoleArn": "arn:aws:iam::886385276223:role/SageMakerExecutionRole",
+                "RoleArn": f"arn:aws:iam::{account_id}:role/SageMakerExecutionRole",
                 "AppSpecification": {
                     "ImageUri": "737474898029.dkr.ecr.us-east-1.amazonaws.com/sagemaker-scikit-learn:0.20.0-cpu-py3"
                 },
